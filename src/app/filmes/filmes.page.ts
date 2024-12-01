8cae557931c650ca993bc5b3799b2f65879fe939
joao-guiilherme <joaoguicaetano02@gmail.com>
2024-12-01 19:58:08 -0300

    Atualizando o codigo

--- src/app/filmes/filmes.page.ts
+++ src/app/filmes/filmes.page.ts
@@ -1,54 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
-  selector: 'app-filmes',
-  templateUrl: './filmes.page.html',
-  styleUrls: ['./filmes.page.scss'],
-})
-export class FilmesPage implements ViewWillEnter {
-  popularMovies: any[] = [];
-
-  constructor(private apiService: ApiService) {}
-
-  ionViewWillEnter() {
-    this.apiService.getPopularMovies().subscribe((data: any) => {
-      console.log('API Response:', data);
-      this.popularMovies = data.results;
-    });
-  }
-  }
-  
-
-
-
-
-
-
-
-
-
-/*import { ViewWillEnter } from '@ionic/angular';
-
-/*@Component({
-  selector: 'app-filmes',
-  templateUrl: './filmes.page.html',
-  styleUrls: ['./filmes.page.scss'],
-})
-export class FilmesPage implements ViewWillEnter {
-  popularMovies: any[] = [];
-
-  constructor(private apiService: ApiService) {}
-
-  ionViewWillEnter() {
-    this.apiService.getPopularMovies().subscribe(data => {
-        this.popularMovies = data.results;
-    });
-  }
-}*/
-
-/*@Component({
   selector: 'app-filmes',
   templateUrl: './filmes.page.html',
   styleUrls: ['./filmes.page.scss'],
@@ -60,7 +13,9 @@ export class FilmesPage implements OnInit {
 
   ngOnInit() {
     this.apiService.getPopularMovies().subscribe((data: any) => {
+      console.log('API Response:', data);
       this.popularMovies = data.results;
+      window.scrollTo(0, 0); // Força o scroll para o topo
     });
   }
-}*/
+}
