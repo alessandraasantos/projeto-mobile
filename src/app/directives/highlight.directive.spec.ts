cee05fb879ec6e88a99631b0321b440f6627a737
jvm_silva <victormendonca0@gmail.com>
2024-12-01 19:41:01 -0300

    adiciona css favoritos

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,17 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
+  let rendererMock: Renderer2;
+  let elRefMock: ElementRef;
+
+  beforeEach(() => {
+    rendererMock = jasmine.createSpyObj('Renderer2', ['setStyle', 'removeStyle']);
+    elRefMock = new ElementRef(document.createElement('div'));
+  });
+
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const directive = new HighlightDirective(elRefMock, rendererMock);
     expect(directive).toBeTruthy();
   });
 });
