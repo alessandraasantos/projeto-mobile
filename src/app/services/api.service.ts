71ba2a7aac0dcf9e964e55e31c20881902e2e439
jvm_silva <victormendonca0@gmail.com>
2024-12-01 15:36:04 -0300

    Adiciona primeiros passos das telas home e filmes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 
@@ -8,13 +8,17 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   private baseUrl = 'https://api.themoviedb.org/3';
-  private apiKey = '1caf0a1170d333c9c7b616e234782ff2';
+  private readonly token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2FmMGExMTcwZDMzM2M5YzdiNjE2ZTIzNDc4MmZmMiIsIm5iZiI6MTczMzA3MjE3Mi42MDcsInN1YiI6IjY3NGM5NTJjMGQ5OWI0NDM2MWUwMmI3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._mlnD0JvaNNwzk7sN6YB7KFTbnEcTlxEIt9mwcG6iKc';
+  private readonly apiKey = '1caf0a1170d333c9c7b616e234782ff2';
 
   constructor(private http: HttpClient) {}
-
+  
   getPopularMovies(): Observable<any> {
     const url = `${this.baseUrl}/movie/popular?api_key=${this.apiKey}`;
-    return this.http.get<any>(url);
+    console.log('Requesting:', url); // Log da URL
+    return this.http.get(url).pipe(
+      tap(response => console.log('API Response:', response)) // Log da resposta
+    );
   }
 
   getMovieDetails(id: string): Observable<any> {
