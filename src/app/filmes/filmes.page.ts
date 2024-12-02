1fe06435012b33a7f3e3491b1d06958f5c7a6501
alessandraasantos <alessandracosmo55@gmail.com>
2024-12-02 18:59:09 -0300

    Adiciona lógica e atualiza template para gerenciar favoritos.

--- src/app/filmes/filmes.page.ts
+++ src/app/filmes/filmes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-filmes',
@@ -8,6 +9,7 @@ import { ApiService } from '../services/api.service';
 })
 export class FilmesPage implements OnInit {
   popularMovies: any[] = [];
+  favorites: any[] = []; // Lista de favoritos
 
   constructor(private apiService: ApiService) {}
 
@@ -16,8 +18,29 @@ export class FilmesPage implements OnInit {
       console.log('API Response:', data);
       this.popularMovies = data.results;
       window.scrollTo(0, 0); // Força o scroll para o topo
+
+      // Carrega favoritos do localStorage
+      const storedFavorites = localStorage.getItem('favorites');
+      this.favorites = storedFavorites ? JSON.parse(storedFavorites) : [];
     });
   }
-  
-  
+
+  // Função para verificar se o filme está nos favoritos
+  isFavorite(movieId: number): boolean {
+    return this.favorites.some((movie) => movie.id === movieId);
+  }
+
+  // Função para remover o filme dos favoritos
+  removeFavorite(movieId: number) {
+    this.favorites = this.favorites.filter((movie) => movie.id !== movieId);
+    localStorage.setItem('favorites', JSON.stringify(this.favorites));
+  }
+
+  // Função para adicionar filme aos favoritos
+  addFavorite(movie: any) {
+    if (!this.isFavorite(movie.id)) {
+      this.favorites.push(movie);
+      localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    }
+  }
 }
